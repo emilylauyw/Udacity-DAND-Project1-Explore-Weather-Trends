1927f97c8f6694fc71cb33af63ee0e4e2aac7da0
Emily Lau <lauyingwen@gmail.com>
2018-04-28 00:08:32 +0800

    Updates to SQL

--- SQL-queries.sql
+++ SQL-queries.sql
@@ -2,20 +2,24 @@
 This file display the commands used to download the CSV files from the online database using SQL.
 */
 
--- Find nearest city which is Singapore
+-- Find the nearest city which is Singapore
 SELECT *
 FROM city_list
 WHERE city LIKE 'Singapore';
 
--- Extract both Singapore and global data by joining the 2 tables and create new columns for the respective average temperature.
-
+/*
+Extract both Singapore and global data by joining the 2 tables and
+create new columns for the respective average temperature.
+*/
 SELECT global_data.year, city_data.city, city_data.avg_temp AS Singapore, global_data.avg_temp AS Global
 FROM city_data JOIN global_data
 ON city_data.year = global_data.year
 WHERE city_data.city LIKE 'Singapore';
 
--- There were missing average temperature data for Singapore from year 1825 to 1838 thus they were removed for this analysis.
-
+/*
+There were missing average temperature data for Singapore from year 1825 to 1838. The missing data
+were removed for this project for better analysis result. 
+*/
 SELECT global_data.year, city_data.city, city_data.avg_temp AS Singapore, global_data.avg_temp AS Global
 FROM city_data JOIN global_data
 ON city_data.year = global_data.year
